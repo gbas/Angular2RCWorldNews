dc550b71606a3c5e9890435d24dfdd5eb7b09579
Giorgos Basagiannis <g.basagiannis@sieben.gr>
2016-06-21 17:32:41 +0300

    initialize

---
+++ app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
+
+//components 
+import {WorldNewsComponent} from './world/worldnews.component';
+import {MoviesComponent} from './movies/movies.component';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app/startpage.html',
+  directives: [ ROUTER_DIRECTIVES],
+  providers : [ROUTER_PROVIDERS]
+})
+
+@RouteConfig([
+  {
+    path: '/worldnews',
+    name: 'WorldNews',
+    component: WorldNewsComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/movies',
+    name: 'Movies',
+    component: MoviesComponent
+  }
+])
+
+
+export class AppComponent {
+
+
+
+
+}
