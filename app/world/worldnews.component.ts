cb8f31a92f609a7ab9b50e808ed772dd007d3e47
gbas <gbas@live.com>
2016-06-28 13:19:30 +0300

    worldnewsComponent modify

--- app/world/worldnews.component.ts
+++ app/world/worldnews.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    template: '<h1>World News Component</h1>'
+    template: '<h1>World News Component in Azure!</h1>'
 })
 
 export class WorldNewsComponent {
